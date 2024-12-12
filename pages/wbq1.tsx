e12071787489b5c5b1e6165ff14a117891bba1ab
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2024-12-12 22:49:13 +0530

    created riddles

--- pages/wbq1.tsx
+++ pages/wbq1.tsx
@@ -3,7 +3,7 @@ import Question from '@/components/word-based-question/Question'
 
 const WBQ1 = () => {
     return (
-        <Question question='this is a question' answer='answer' />
+        <Question question='A mathematician encoded a secret message in a series of 3-digit numbers, each corresponding to a specific chemical element. Each number represents the atomic mass of an element rounded to the nearest whole number. When you sum the atomic masses, the result equals the 10th Fibonacci number. Identify the elements and decipher the hidden message formed by their symbols.' answer='HONOR' />
     )
 }
 
--- pages/wbq3.tsx
+++ pages/wbq3.tsx
@@ -3,7 +3,7 @@ import Question from '@/components/word-based-question/Question'
 
 const WBQ3 = () => {
   return (
-    <Question question='this is a question3' answer='answer' />
+    <Question question='An archaeologist discovers a scroll containing a sequence of letters: A,G,C,T, and observes that they follow a DNA-like double helix pattern. The sequence encodes a set of geographic coordinates. However, the scroll notes that the sequence must first be transcribed using Morse code, then converted to binary, and finally analyzed using a formula for elliptical orbits in physics. Solve for the location and identify its historical significance.' answer='Giza' />
   )
 }
 
