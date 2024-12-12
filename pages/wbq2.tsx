043db29c4e13a6703bc52b67c7687ed8c94079f1
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2024-12-12 23:11:39 +0530

    updated 1 question answer

--- pages/wbq2.tsx
+++ pages/wbq2.tsx
@@ -3,7 +3,7 @@ import Question from '@/components/word-based-question/Question'
 
 const WBQ2 = () => {
   return (
-    <Question question='A clock strikes the hour based on the Fibonacci sequence, with the number of chimes equal to the sequence’s position in hours (1 for 1st hour, 1 for 2nd hour, 2 for 3rd hour, etc.). At exactly 3:21 PM, the hands of the clock align perfectly, pointing to a 3-digit prime number if viewed as angles. Calculate this prime number and determine which planet in the solar system has a corresponding orbital period (in Earth days) closest to it.' answer='88 days' />
+    <Question question='A clock strikes the hour based on the Fibonacci sequence, with the number of chimes equal to the sequence’s position in hours (1 for 1st hour, 1 for 2nd hour, 2 for 3rd hour, etc.). At exactly 3:21 PM, the hands of the clock align perfectly, pointing to a 3-digit prime number if viewed as angles. Calculate this prime number and determine which planet in the solar system has a corresponding orbital period (in Earth days) closest to it.' answer='113 Mercury' />
   )
 }
 
